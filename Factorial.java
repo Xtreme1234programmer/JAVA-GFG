685eb333a1cbfaf3b57a2ec58026c8a942b0d2c4
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-06-20 21:23:29 +0530

    Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,6 @@
+ Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number to find the factorial::");
+        int n=sc.nextInt();
+
+        int ans=calculateFactorial(n);
+        System.out.println("The factorial of given number is:: "+ ans);
