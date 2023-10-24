88ca619a287e50e5c6a8e3b8ee9f93f03d13577d
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2023-10-24 19:11:35 +0530

    Create Missing_element_in_AP.java

---
+++ Missing_element_in_AP.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMissing(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    int findMissing(int[] arr, int n) {
+        // code here
+        int d=(arr[n-1]-arr[0])/n;
+        int res=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i+1]-arr[i]!=d) res=arr[i]+d;
+        }
+        return res;
+    }
+}
