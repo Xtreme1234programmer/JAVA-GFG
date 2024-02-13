cf540e1f011d72f0a613761ff1e7c301ed4b5cd6
Pankaj singh negi <99879926+Xtreme1234programmer@users.noreply.github.com>
2024-02-14 01:07:59 +0530

    Create Peak_element.java

---
+++ Peak_element.java
@@ -0,0 +1,25 @@
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       //add code here.
+       int low=0;
+       int high=n-1;
+       
+       while(low<=high)
+       {
+           int mid=(low+high)/2;
+           
+           if( (mid == 0 || arr[mid-1] <= arr[mid]) && 
+                (mid == n-1 || arr[mid+1] <= arr[mid]) ) return mid;
+           
+           else if(arr[mid+1]>=arr[mid]) {low=mid+1;}
+           else
+           {high=mid-1;}
+       }
+       return -1;
+    }
+}
